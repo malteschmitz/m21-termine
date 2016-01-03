cbc1591cb4b3166c23ffa294cf681679a8d7a3bc
Malte Schmitz <malte@schmitz-sh.de>
2016-01-03 20:10:03 +0100

    Fix convert and really remove commas

--- convert.rb
+++ convert.rb
@@ -1,13 +1,15 @@
 require 'csv'
 
-# Converts semi-colon separated values to comma separated values (real csv)
+# To converts semi-colon separated values to comma separated values (real csv)
+# add this option to CSV.read: `:col_sep => ';'`
 
-data = CSV.read(ARGV[0], :col_sep => ';', :row_sep => :auto, :encoding => 'utf-8')
+# Remove `,` from CSV files
+data = CSV.read(ARGV[0], :encoding => 'utf-8')
 CSV.open(ARGV[0], "wb", :encoding => 'utf-8') do |csv|
   data.each do |row|
     new_row = row.map do |cell|
       cell ||= ""
-      cell.gsub(',', ';')
+      cell.gsub!(',', ' /')
       cell.empty? ? nil : cell
     end
     csv << new_row
